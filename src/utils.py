5ecf12f9c3aa40319191daba9679d991d737cb30
el98006 <el98006@gmail.com>
2016-11-04 16:12:33 -0700

    boilerplate for get serivce state variable and actions

--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,71 @@ Created on Nov 2, 2016
 
 @author: eli
 '''
-import urllib
+import urllib2
 import re
 import xml.etree.ElementTree as xt
+from idlelib.idle_test.mock_tk import Event
 
-UDNP_SERVICE ="urn:schemas-upnp-org:service"
+UDNP_SERVICE ='urn:schemas-upnp-org:service'
+SUB_MSG_TEMPLATE = ['SUBSCRIBE {} HTTP/1.1',  
+'HOST: {}',
+'CALLBACK: {}',
+'NT: upnp:Event',
+]
+
+class upnp_action:
+    def init(self, action_name):
+        self.name = action_name
+        
+class upnp_servcie:
+    def init(self):
+        self.id = ''
+        self.service_type = ''
+        self.control_url = ''
+        self.event_sub_url =''
+        self.scpdurl = ''
+    
+    def set_type(self, type):
+        self.service_type = type
+    
+    def set_control_url(self,url):
+        self.control_url = url
+    
+    def set_event_sub_url(self, url):
+        self.event_sub_url = url
+    
+    def set_scpdurl(self, url):
+        self.scpdurl = url
+    
+    def get_service_actions(self):
+        '''
+        retrieve available list of actions by querying scpdurl
+        return 
+        stateTable and actionLists
+        stableTable: name, dataType, allowedValue
+        action: name, argumentList, 
+        argument: name, direciton, relatedStableVariable
+        
+        
+        '''
+        
+    def subscribe_to_events(self, receiving_url):
+        fh = urllib2.urlopen(self.event_sub_url)
+        '''
+        message = 
+        header = 
+        resp = fh.post()
+        resp contains names and current values of all evented variables
+        '''
+    def renew_event_subscription(self):
+        
+        
+        
+        
+         
+    
+
+
 
 def print_element(item):
     #service_value = item.text.encode('utf8').rsplit(':',2)[1]
