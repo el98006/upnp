98bcf161b24e3886f69b42a8262c5bf0b95f89a3
el98006 <el98006>
2016-11-17 12:35:01 -0800

    add templates

--- src/server.py
+++ src/server.py
@@ -3,9 +3,10 @@ Created on Nov 11, 2016
 
 @author: eric
 '''
-from flask import Flask,send_from_directory,request
+from flask import Flask,send_from_directory,request, render_template
 import os
-from flask import Response
+
+#from flask import Response
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -15,19 +16,26 @@ mime_map = {'.png':'image/png','.xpm':'image/xpm'}
 def index():
     my_dir = os.path.dirname(__file__)
     file_path = os.path.join(my_dir, 'pics')
-    #mimetype = mime_map['.png']
-    #res = Response(mimetype = mimetype)
-    #return(res)
     return(send_from_directory(file_path, 'firefox.png'))
  
-@app.route('/upload', method='POST')
+@app.route('/upload', methods=['GET','POST'])
 def upload():
-    file = request.files['file']
-    if file.filename:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(os.path.dirname(__file__),filename))
-        #return (render_template('index.html'),filename=filename)
-    
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename:
+            fname = secure_filename(file.filename)
+            my_dir = os.path.dirname(__file__)
+            file_path = os.path.join(my_dir, 'pics')
+            file.save(os.path.join(file_path,fname))
+            return render_template('index.html', filename=fname)
+    return render_template('index.html')
 
+@app.route('/store')
+def browse_stored_file(filename): 
+    my_dir = os.path.dirname(__file__)
+    file_path = os.path.join(my_dir, 'pics')
+    file_list = [ item for item in os.listdir(file_path) ]
+    return(render_template('list.html', file_list))
+    
 if __name__ == '__main__':
     app.run(debug=True)
