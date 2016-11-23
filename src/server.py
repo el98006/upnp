7616f7b8530c261f88c9d989e12e8d24dbf053ae
el98006 <el98006>
2016-11-23 13:12:55 -0800

    fix routing bugs

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ Created on Nov 11, 2016
 
 @author: eric
 '''
-from flask import Flask,send_from_directory,request, render_template, url_for
+from flask import Flask,send_from_directory,request, render_template, url_for, Response
 import os
 
 #from flask import Response
@@ -12,12 +12,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 mime_map = {'.png':'image/png','.xpm':'image/xpm'}
 
-@app.route('/')
-def index():
-    my_dir = os.path.dirname(__file__)
-    file_path = os.path.join(my_dir, 'pics')
-    return(send_from_directory(file_path, 'firefox.png'))
- 
+
 @app.route('/upload', methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
@@ -25,18 +20,23 @@ def upload():
         if file.filename:
             fname = secure_filename(file.filename)
             my_dir = os.path.dirname(__file__)
-            file_path = os.path.join(my_dir, 'pics')
+            file_path = os.path.join(my_dir, 'media')
             file.save(os.path.join(file_path,fname))
             return render_template('index.html', filename=fname)
     return render_template('index.html')
 
-@app.route('/list')
-def browse_stored_file(): 
+@app.route('/media/<path:filename>')
+def get_media(filename):
+    my_dir = os.path.dirname(__file__)
+    file_path = os.path.join(my_dir, 'media')
+    return(send_from_directory(file_path, filename))
+
+@app.route('/')
+def browse_uploaded_files(): 
     
     my_dir = os.path.dirname(__file__)
-    file_path = os.path.join(my_dir, 'pics')
-    #file_list = [ os.path.join(my_dir, item)  for item in os.listdir(file_path) ]
-    file_list = [ url_for('static', filename=item)  for item in os.listdir(file_path) ]
+    file_path = os.path.join(my_dir, 'media')
+    file_list = [ item  for item in os.listdir(file_path) ]
     
     return(render_template('list.html', file_list=file_list))
     
