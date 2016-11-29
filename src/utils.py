29bfa258add314a7db067eb9ae6f46bfefc7b315
el98006 <el98006@gmail.com>
2016-11-29 15:49:33 -0800

    fix merge problems

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 '''
+updated on 11/29
 Created on Nov 2, 2016
 
 @author: eli
@@ -7,60 +8,44 @@ import urllib2
 import re
 import xml.etree.ElementTree as xt
 
-import xml
 
 
-SERVICE_NAMESPACE ='urn:schemas-upnp-org:service'
-SERVICE_VER = '1'
 
 A_ARG_TYPE_InstanceID = 1
 DEFAULT_PLAY_SPEED = 1
->>>>>>> 5ec26aed319fbeb833a8d0816da0d93faf6dee53
-
-SERVICE_NAMESPACE ='urn:schemas-upnp-org:service'
 SERVICE_VER = '1'
 
-A_ARG_TYPE_InstanceID = 1
-DEFAULT_PLAY_SPEED = 1
+NAME_SPACE = {'service_type': 'urn:schemas-upnp-org:service', 'service_id':'urn:upnp-org'}
+SERVICE_NAMESPACE ='urn:schemas-upnp-org:service-1-0'
 
+SOAP_HEADER_TEMPLATE = {
+    'Content-Type' : 'text/xml; charset="utf-8"',
+    "Content-Length": "",
+    "Connection"     : "close",
+    "SOAPACTION"     : ""
+} 
 
+SOAP_BODY_TEMPLATE ='<?xml version="1.0"?>\
+                    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\
+                    <s:Body> \
+                    <u:{actionName} xmlns:u="urn:schemas-upnp-org:service:{serviceType}:{version}">\
+                    <argumentName>{arguments}</argumentName> \
+                    </u:{actionName}>\
+                    </s:Body>\
+                    </s:Envelope>' 
+ 
 
+SOAPACTION_TEMPLATE = "urn:schemas-upnp-org:{service}:{serviceType}:{v}#{actionName}"
 
-
-
-                    </s:Envelope>'  
-
-SOAPACTION_TEMPLATE = "urn:schemas-upnp-org:{service}:{serviceType}:v#{actionName}"
 SUB_MSG_TEMPLATE = ['SUBSCRIBE {} HTTP/1.1',  
 'HOST: {}',
 'CALLBACK: {}',
 'NT: upnp:Event',
 ]
               
-NAME_SPACE = {'service_type': 'urn:schemas-upnp-org:service', 'service_id':'urn:upnp-org'}
-                    
-def extrac_host_info(url):
-    pattern = re.compile('http\:\/\/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(\:\d+)?')
-    
-    m = re.match(pattern, url)
-    if m:
-        host_ip_port = m.group(0)
-    else:
-        host_ip_port = ''
-    return host_ip_port
 
 
-
-SOAPACTION_TEMPLATE = "urn:schemas-upnp-org:{service}:{serviceType}:v#{actionName}"
-SUB_MSG_TEMPLATE = ['SUBSCRIBE {} HTTP/1.1',  
-'HOST: {}',
-'CALLBACK: {}',
-'NT: upnp:Event',
-]
-              
-NAME_SPACE = {'service_type': 'urn:schemas-upnp-org:service', 'service_id':'urn:upnp-org'}
-
-def extrac_host_info(url):
+def extract_host_info(url):
     pattern = re.compile('http\:\/\/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(\:\d+)?')
     
     m = re.match(pattern, url)
@@ -71,13 +56,6 @@ def extrac_host_info(url):
     return host_ip_port
 
 class upnp_action:
-    '''
-    action_name: Play
-    argument: name = InstanceID,  direction: in, relatedStateVariable: A_ARG_TYPE_InstanceID;
-    name: Speed, direction: in, relatedStateVariable: TransportPlaySpeed
-    
-    methods: execute()
-    '''
     
     def __init__(self, action_name=''):
         self.name = action_name
@@ -97,16 +75,16 @@ class upnp_service:
     control_url = /upnp/control/AVTransport1
     eventSubURL = /upnp/event/AVTransport1
     '''
-    def __init__(self, service_type, id):
-        self.id = id
+    def __init__(self, service_type, service_id):
+        self.service_id = service_id
         self.service_type = service_type
         self.control_url = ''
         self.event_sub_url =''
         self.scpdurl = ''
         self.action_list = {}
     
-    def set_type(self, type):
-        self.service_type = type
+    def set_type(self, service_type):
+        self.service_type = service_type
     
     def set_control_url(self,url):
         self.control_url = url
@@ -130,73 +108,73 @@ class upnp_service:
         
         fh = urllib2.urlopen(self.scpdurl)
         resp = fh.read()
-        '''
-        with open('AVTransport1.xml', 'rt') as xml:
-            resp = xml.read()
-        '''
         resp = re.sub(' xmlns=\"[^\"]+\"','', resp, 1)
         root = xt.fromstring(resp)
-        for item in root.findall('./actionList/action'):
-            action_name = item.find('name')
+        for item in root.findall('actionList/action'):
+        #for item in root.findall('./actionList/action'):
+            try: 
+                action_name = item.find('name')
+            except:
+                continue
             new_action = upnp_action(action_name.text)
             print new_action
             self.action_list[action_name] = new_action
     
     def __repr__(self):
 
-        return 'id:{} type:{} \n scpdurl:{}, control_url:{}'.format(self.id, self.service_type, self.scpdurl, self.control_url )
+        return 'id:{} type:{} \n scpdurl:{}, control_url:{}'.format(self.service_id, self.service_type, self.scpdurl, self.control_url )
     
     def AV_capapable(self):
-        return (self.service_type == 'AVTransport:1')
+        return (self.service_type == ':AVTransport:1')
 
-    def invoke_SetAVTransportURI(self):
-        header_soapaction = SOAPACTION_TEMPLATE.format(self.service,self.seviceType,SERVICE_VER, 'SetAVTransportURI')
-        headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
-        resp = urllib2.Request(self.control_url, play_content, headers)
-        ret = resp.read()
-        '''<CurrentURI> url to the media </CurrentURI> '''
-        headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
-        resp = urllib2.Request(self.control_url, play_content, headers)
-        ret = resp.read()
+
         
         
     def subscribe_to_events(self, receiving_url):
         fh = urllib2.urlopen(self.event_sub_url)
-        '''
-        message = 
-        header = 
-        resp = fh.post()
-        resp contains names and current values of all evented variables
-        '''
-    #def renew_event_subscription(self):
+      
+    
         
         
         
     def invoke_PLAY(self):
-        argument = {}
-        play_action = self.action_list['Play']
-        argument['InstanceID'] = 0
-        argument['Speed'] = DEFAULT_PLAY_SPEED
-        play_content = "http://abc"
-        content_len = len(play_content)
+        args = {}
+        args['InstanceID'] = 0
+        args['Speed'] = DEFAULT_PLAY_SPEED   
+        msg_body = SOAP_BODY_TEMPLATE() 
         
-        header_soapaction = SOAPACTION_TEMPLATE.format(self.service,self.seviceType,SERVICE_VER, 'Play')
-        headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
-        resp = urllib2.Request(self.control_url, play_content, headers=header_soapaction)
-        ret = resp.read()   
+        header_soapaction = SOAPACTION_TEMPLATE.format(service=self.service_id,serviceType=self.service_type, v=SERVICE_VER, serviceAction='Play')
+        header = SOAP_HEADER_TEMPLATE
+        header['Content-Length'] = len(msg_body)
+        header['SOAPACTION'] = header_soapaction
+        
+       
+        resp = urllib2.Request(self.control_url, data=msg_body, headers=header)
+        ret = urllib2.urlopen(resp)
+        ret_msg = ret.read()
+        print ret_msg
+        
+        
          
     def invoke_SetAVTransportURI(self):
-        header_soapaction = SOAPACTION_TEMPLATE.format(self.service,self.seviceType,SERVICE_VER, 'SetAVTransportURI')
-        play_content = "http:: point to the media file"
-        content_len  = len(play_content)
-        headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
-        resp = urllib2.Request(self.control_url, play_content, headers)
-        ret = resp.read()
-        '''<CurrentURI> url to the media </CurrentURI> '''
-        headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
-        resp = urllib2.Request(self.control_url, play_content, headers)
-        ret = resp.read()
-
+        
+        args = {}
+        header_soapaction = SOAPACTION_TEMPLATE.format(service=self.service_id,serviceType=self.seviceType,v=SERVICE_VER, serviceAction='SetAVTransportURI')
+        target_uri = 'http://localhost:5000/media/flower.jpg'
+        
+        args['InstanceID'] = 0
+        args['CurrentURI'] = target_uri
+        
+        msg_body = SOAP_BODY_TEMPLATE.format(actionName='SetAVTransportURI', serviceType='AVTransport', version='1', arguments=args)
+              
+        SOAP_HEADER_TEMPLATE['Content-Length'] = len(msg_body)
+        SOAP_HEADER_TEMPLATE['SOAPACTION'] = header_soapaction
+       
+        resp = urllib2.Request(self.control_url, data=msg_body, headers=SOAP_HEADER_TEMPLATE)
+        ret = urllib2.urlopen(resp)
+        ret_msg = ret.red()
+        print ret_msg
+        
 
 
 def xml_to_info(xml, root_url):
@@ -222,7 +200,7 @@ def xml_to_info(xml, root_url):
         service_id = re.sub(NAME_SPACE['service_id'], '',s_type )
 
         service_node = upnp_service(s_type, service_id)
-        host_ip_port = extrac_host_info(root_url)
+        host_ip_port = extract_host_info(root_url)
         scpd_url = host_ip_port + s.find('SCPDURL').text
         
         control_url = host_ip_port + s.find('controlURL').text
@@ -238,10 +216,3 @@ def xml_to_info(xml, root_url):
 
         
     
-'''
-
-if __name__ == '__main__':
-    s = upnp_service('AVTransport:1', 'AVTransport:1')
-    s.set_control_url('/xml/AVTransport1')
-    s.get_service_actions()
-'''
