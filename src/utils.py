77e3215741909e2939e14deba38c74807acec2fd
el98006 <el98006@gmail.com>
2016-11-29 09:46:45 -0800

    local merge dev and master

--- src/utils.py
+++ src/utils.py
@@ -6,9 +6,9 @@ Created on Nov 2, 2016
 import urllib2
 import re
 import xml.etree.ElementTree as xt
-<<<<<<< HEAD
+
 import xml
-=======
+
 
 SERVICE_NAMESPACE ='urn:schemas-upnp-org:service'
 SERVICE_VER = '1'
@@ -23,26 +23,11 @@ SERVICE_VER = '1'
 A_ARG_TYPE_InstanceID = 1
 DEFAULT_PLAY_SPEED = 1
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 5ec26aed319fbeb833a8d0816da0d93faf6dee53
-SOAP_HEADER_TEMPLATE = {
-    "Content-Type"   : "text/xml; charset=\"utf-8\"",
-    "Content-Length" : "{}",
-    "Connection"     : "close",
-    "SOAPACTION"     : "{}"
-} 
-SOAP_BODY_TEMPLATE ='<?xml version="1.0"?>\
-                    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\
-                    <s:Body> <u:actionName xmlns:u="urn:schemas-upnp-org:service:serviceType:v">\
-                    <argumentName>{}</argumentName> \
-                    </u:actionName>\
-                    </s:Body>\
-<<<<<<< HEAD
-                    </s:Envelope>' 
-=======
+
+
+
                     </s:Envelope>'  
 
 SOAPACTION_TEMPLATE = "urn:schemas-upnp-org:{service}:{serviceType}:v#{actionName}"
@@ -63,7 +48,7 @@ def extrac_host_info(url):
     else:
         host_ip_port = ''
     return host_ip_port
->>>>>>> 5ec26aed319fbeb833a8d0816da0d93faf6dee53
+
 
 
 SOAPACTION_TEMPLATE = "urn:schemas-upnp-org:{service}:{serviceType}:v#{actionName}"
@@ -158,27 +143,12 @@ class upnp_service:
             self.action_list[action_name] = new_action
     
     def __repr__(self):
-<<<<<<< HEAD
-        return 'service id:{} type:{}\nscpdurl:{}\ncontrol_url:{}\n'.format(self.id, self.service_type, self.scpdurl, self.control_url )
-    
-    def invoke_PLAY(self):
-        argument = {}
-        play_action = self.action_list['Play']
-        argument['InstanceID'] = 0
-        argument['Speed'] = DEFAULT_PLAY_SPEED
-        contentent_len = 0
-        header_soapaction = SOAPACTION_TEMPLATE.format(self.service,self.seviceType,SERVICE_VER, 'Play')
-        headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
-        resp = urllib2.Request(self.control_url, play_content, headers=header_soapaction)
-        ret = resp.read()
-=======
+
         return 'id:{} type:{} \n scpdurl:{}, control_url:{}'.format(self.id, self.service_type, self.scpdurl, self.control_url )
     
     def AV_capapable(self):
         return (self.service_type == 'AVTransport:1')
-        
->>>>>>> 5ec26aed319fbeb833a8d0816da0d93faf6dee53
-        
+
     def invoke_SetAVTransportURI(self):
         header_soapaction = SOAPACTION_TEMPLATE.format(self.service,self.seviceType,SERVICE_VER, 'SetAVTransportURI')
         headers = SOAP_HEADER_TEMPLATE.format(content_len, header_soapaction )
@@ -260,12 +230,8 @@ def xml_to_info(xml, root_url):
         service_node.set_control_url(control_url)
         
         print(service_node)
-<<<<<<< HEAD
-        
-        service_node.get_service_actions()
-        service_list.append(service_node)
-=======
->>>>>>> 5ec26aed319fbeb833a8d0816da0d93faf6dee53
+
+
         
         service_node.get_service_actions()
         service_list.append(service_node)        
